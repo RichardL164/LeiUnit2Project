df883af76e51a78631275dbb83d12365f9cc5fd5
BT_2N2_33 <sample@bths.edu>
2023-10-16 14:03:25 -0400

    Initial Commit

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class LinearEquation {
+public class LinearEquation {
+    private int x1;
+    private int x2;
+    private int y1;
+    private int y2;
+
+    public LinearEquation(int x1, int x2, int y1, int y2) {
+        this.x1 = x1;
+        this.x2 = x2;
+        this.y1 = y1;
+        this.y2 = y2;
+    }
+    public String lineEquation(String coordinate1, String coordinate2) {
+        double slope = ((double) (y2 - y1) / (x2 - x1));
+
+    }
 }
--- src/LinearEquationLogic.java
+++ src/LinearEquationLogic.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class LinearEquationLogic {
+import java.util.Scanner;
+public class LinearEquationLogic {
+    private Scanner scan;
+    String coordinate1;
+    String coordinate2;
+    private LinearEquation line;
+
+    public LinearEquationLogic(String coordinate1, String coordinate2) {
+        scan = new Scanner(System.in);
+        line = null;
+        this.coordinate1 = coordinate1;
+        this.coordinate2 = coordinate2;
+    }
+
+
+    public void getCoordinates() {
+        System.out.println("Welcome to the linear equation calculator!");
+        System.out.print("Enter coordinate 1: ");
+        coordinate1 = scan.nextLine();
+        System.out.print("Enter coordinate 2: ");
+        coordinate2 = scan.nextLine();
+    }
+    public void getPoints() {
+        int commaIndex1 = coordinate1.indexOf(",");
+        int commaIndex2 = coordinate2.indexOf(",");
+        int coordinate1Length = coordinate1.length();
+        int coordinate2Length = coordinate2.length();
+        int x1 = Integer.parseInt(coordinate1.substring(0, commaIndex1));
+        int x2 = Integer.parseInt(coordinate2.substring(0, commaIndex2));
+        int y1 = Integer.parseInt(coordinate1.substring(commaIndex1 + 2, coordinate1Length));
+        int y2 = Integer.parseInt(coordinate2.substring(commaIndex2 + 2, coordinate2Length));
+        line = new LinearEquation(x1, x2, y1, y2);
+    }
 }
