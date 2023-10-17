5c94d238bfbc83c5e135453cdb78b9e1d1c2193d
BT_2N2_33 <sample@bths.edu>
2023-10-17 14:07:29 -0400

    Finished upto freestyle, have to make new constructor with two parameters

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -3,8 +3,6 @@ public class LinearEquation {
     private int x2;
     private int y1;
     private int y2;
-    private double slope;
-    private double yIntercept;
 
     public LinearEquation(int x1, int x2, int y1, int y2) {
         this.x1 = x1;
@@ -12,20 +10,39 @@ public class LinearEquation {
         this.y1 = y1;
         this.y2 = y2;
     }
-    public String equation(String coordinate1, String coordinate2) {
-        return "y = " + slope + "x + " + yIntercept;
+    public double roundToHundredth(double toRound) {
+        return Math.round(toRound * 100) / 100.0;
+    }
+    public String equation() {
+        return "y = " + slope() + "x + " + yIntercept();
     }
     public double slope() {
-        slope = ((double) (y2 - y1) / (x2 - x1));
-        return slope;
+        double slope = (double) (y2 - y1) / (x2 - x1);
+        return roundToHundredth(slope);
     }
     public double yIntercept() {
-        yIntercept = y1 - slope * x1;
-        return yIntercept;
+        double yIntercept = y1 - slope() * x1;
+        return roundToHundredth(yIntercept);
     }
     public double distance() {
         double distanceSquared = Math.pow((x1 - x2), 2) + Math.pow((y1 - y2), 2);
         double distance = Math.pow(distanceSquared, 0.5);
-        return Math.round(distance * 100) / 100.0;
+        return roundToHundredth(distance);
+    }
+    public String coordinateForX(double x) {
+        double yCoord = x * slope() + yIntercept();
+        return "The point on the line is: (" + x + ", " + roundToHundredth(yCoord) + ")";
+    }
+    public void lineInfo() {
+        if (x1 != x2) {
+            System.out.println("The two points are: (" + x1 + ", " + y1 + ") and (" + x2 + ", " + y2 + ")");
+            System.out.println("The equation of the line between these two points is: " + equation());
+            System.out.println("The slope of this line is: " + slope());
+            System.out.println("The y-intercept of this line is: " + yIntercept());
+            System.out.println("The distance between these points is: " + distance());
+        }
+        else {
+            System.out.println("These points are on a vertical line x = " + x1);
+        }
     }
 }
--- src/LinearEquationLogic.java
+++ src/LinearEquationLogic.java
@@ -11,10 +11,21 @@ public class LinearEquationLogic {
         this.coordinate1 = coordinate1;
         this.coordinate2 = coordinate2;
     }
-
-
-    public void getCoordinates() {
+    public void start() {
         System.out.println("Welcome to the linear equation calculator!");
+        getCoordinates();
+        getPoints();
+        line.lineInfo();
+        coordinatePair();
+        continueGame();
+        while (continueGame()) {
+            getCoordinates();
+            getPoints();
+            line.lineInfo();
+            coordinatePair();
+        }
+    }
+    public void getCoordinates() {
         System.out.print("Enter coordinate 1: ");
         coordinate1 = scan.nextLine();
         System.out.print("Enter coordinate 2: ");
@@ -25,15 +36,22 @@ public class LinearEquationLogic {
         int commaIndex2 = coordinate2.indexOf(",");
         int coordinate1Length = coordinate1.length();
         int coordinate2Length = coordinate2.length();
-        int x1 = Integer.parseInt(coordinate1.substring(0, commaIndex1));
-        int x2 = Integer.parseInt(coordinate2.substring(0, commaIndex2));
-        int y1 = Integer.parseInt(coordinate1.substring(commaIndex1 + 2, coordinate1Length));
-        int y2 = Integer.parseInt(coordinate2.substring(commaIndex2 + 2, coordinate2Length));
+        int x1 = Integer.parseInt(coordinate1.substring(1, commaIndex1));
+        int x2 = Integer.parseInt(coordinate2.substring(1, commaIndex2));
+        int y1 = Integer.parseInt(coordinate1.substring(commaIndex1 + 2, coordinate1Length - 1));
+        int y2 = Integer.parseInt(coordinate2.substring(commaIndex2 + 2, coordinate2Length - 1));
         line = new LinearEquation(x1, x2, y1, y2);
     }
     public void coordinatePair() {
         System.out.println("Enter a value for x: ");
-        String xCoord = scan.nextLine();
-
+        double xCoord = scan.nextDouble();
+        System.out.println(line.coordinateForX(xCoord));
+    }
+    public boolean continueGame() {
+        System.out.print("Would you like to enter another pair of coordinates? y/n: ");
+        String continueGame = scan.nextLine();
+        if (continueGame.equals("y")) {
+            return true;
+        } return false;
     }
 }
